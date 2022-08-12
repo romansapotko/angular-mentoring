1450bff011bf62175a3819aaa67b78c83963d38b
Roman_Sapotko <roman_sapotko@epam.com>
2022-08-12 13:55:42 +0300

    Adjust date

--- src/app/course-management/utils/duration.pipe.ts
+++ src/app/course-management/utils/duration.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'duration',
 })
 export class DurationPipe implements PipeTransform {
-  transform(value: number, ...args: unknown[]): string {
+  transform(value: number): string {
     const hours = Math.floor(value / 60);
     const minutes: number = value - hours * 60;
 
